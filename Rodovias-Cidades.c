2fc3c37f6a758ee5d9cd5fdd422180508f3f54b9
ErickFraga <37560574+ErickFraga@users.noreply.github.com>
2019-04-01 18:48:25 -0300

    Update Rodovias-Cidades.c

--- Rodovias-Cidades.c
+++ Rodovias-Cidades.c
@@ -397,6 +397,90 @@ void posListaDE(ListaDE lstDE, int pos)
     }
 }
 
+int readCitiesAndRoadsFromFile(FILE *fCities,  FILE *fRoads,  ListaSE lstRodovias){
+    #define MAXCHAR 1000
+    #define MAX 100
+    char buffer[MAXCHAR]; // buffer for a line of cities separated by spaces
+    char cidade[MAX]; // save the name of a city
+    char Str[MAX];
+
+    //open files
+    fCities = fopen("/home/ubuntu/workspace/cities", "r");
+    fRoads = fopen("/home/ubuntu/workspace/roads", "r");
+
+    if (fCities == NULL) {
+        printf("\n It was not possible to open file /home/ubuntu/workspace/cities \n");
+        return 0;
+
+    }
+
+     if (fRoads == NULL){
+
+        printf("\n It was not possible to open file /home/ubuntu/workspace/roads \n");
+        return 0;
+    }
+
+
+    while(!feof(fRoads)){
+
+        memset(buffer, '\0', 1000); // setting '\0' for whole buffer
+
+        InfoRodovia elem; // save InfoRodovia for a new element
+
+        elem.cidades = inicListaDE(); // intialize Doubly Linked List of cities of the currente element
+
+        // copy road name
+        fscanf(fRoads, "%s", elem.rodovia);
+        printf("\n -----------------------------(%s)--------------------------------- \n", elem.rodovia);
+
+        fgets(buffer, MAXCHAR, fCities); // reads a line of cities to the buffer
+        printf("\n %s \n ", buffer);
+
+        int i=0, j=0;
+		memset(cidade, '\0', 100);
+
+		while (1){
+
+			while( !isspace(buffer[i]) && buffer[i] != '\0'){
+				cidade[j] = buffer[i];
+				j++; // next free position for a letter (char)
+				i++;
+			}
+			//printf("\n cidade:  %s \n", cidade);
+
+			//add city to the list of cities
+            anxListaDE(elem.cidades, cidade);
+
+			if(buffer[i] == '\0')
+				break; //acabou de ler todas as cidades de uma rodovia
+
+			i++; //pula o espaco
+			j=0; //reset j so it begins from 0
+			memset(cidade, '\0', 100); //reset cidade
+
+		}
+
+		//printListaDE(elem.cidades);
+
+		anxListaSE(lstRodovias, elem);
+
+    }
+    fclose(fCities);
+    fclose(fRoads);
+
+    return 1; //all read successfully
+
+}
+
+
+ListaDE rodoviasCidade(ListaSE lstRodovias, char * cidade){
+
+    ListaDE rodCortamCidade = inicListaDE();
+
+    // finalizar
+    return rodCortamCidade;
+}
+
 char infoListaDE(ListaDE lstDE)
 {
     if(lstDE->iterador == NULL)
@@ -423,19 +507,18 @@ int fimListaDE(ListaDE lstDE)
 
 ListaDE rodoviasCidade(ListaSE lstRodovias, char * cidade){
 
-    ListaDE rodCortamCidade = inicListaDE();
-
-    InfoRodovia rodoviaIterador;
+    ListaDE rodCortamCidade = inicListaDE(); // Lista de rodovias que sera retornada
+    InfoRodovia rodoviaElemento; // Campo info rodovia auxiliar
 
     for(primListaSE(lstRodovias); !fimListaSE(lstRodovias); segListaSE(lstRodovias)){
+    // For que percorre a lista de rodovias
+        rodoviaElemento = infoListaSE(lstRodovias); // Recebe o info rodovia de cada Lista
 
-        rodoviaIterador = infoListaSE(lstRodovias);
-
-
-
-        for(primListaDE(rodoviaIterador.CidadesED);!fimListaDE(rodoviaIterador.CidadesED); segListaDE(rodoviaIterador.CidadesED, 1)){
-            if(strcmp(infoListaDE(rodoviaIterador.CidadesED), cidade) == 0){
-                anxListaDE(rodCortamCidade ,infoListaDE(rodoviaIterador.CidadesED));
+        for(primListaDE(rodoviaElemento.CidadesED);!fimListaDE(rodoviaElemento.CidadesED); segListaDE(rodoviaElemento.CidadesED, 1)){
+        // Percorre a lista de cidades dentro de cada rodovia
+            if(strcmp(infoListaDE(rodoviaElemento.CidadesED), cidade) == 0){
+            //compara o nome buscado com o  nome de cada cidade
+                anxListaDE(rodCortamCidade ,infoListaDE(rodoviaElemento.CidadesED)); // Caso o nomeseja ogual adiciona a lista para retornar
             }
         }
     }
